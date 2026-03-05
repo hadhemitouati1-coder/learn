252fd6f9023571659832b3772a58e364d0a91ab9
hadhem itouati <hadhemitouati1@gmail.com>
2026-03-06 00:36:22 +0100

    second dev

---
+++ first.js
@@ -0,0 +1,9 @@
+const names = ["hadhemi", "rahma ", "dali "];
+function printNames(name) {
+    
+  names.forEach((name) => {
+    console.log(name);
+  });
+}
+printNames();
+names.map((printNames) => {});
